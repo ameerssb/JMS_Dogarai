4484bdf21f4e397fdf1e03b1faa040990302798e
Sani Bature Sufyan <sanisbature17@gmail.com>
2023-05-30 20:51:01 +0100

    new folder deleted

--- New folder/verify_email/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth import get_user_model
-
-
-class RequestNewVerificationEmail(forms.Form):
-    email = forms.EmailField(
-        label=get_user_model()._meta.get_field("email").verbose_name,  # noqa
-        help_text=get_user_model()._meta.get_field("email").help_text,  # noqa
-    )
