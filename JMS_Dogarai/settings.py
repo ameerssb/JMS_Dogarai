57da636a1cf945fabf908fb05e63bf99d076ee26
Sani Bature Sufyan <sanisbature17@gmail.com>
2023-05-30 21:25:12 +0100

    Update settings.py

--- JMS_Dogarai/settings.py
+++ JMS_Dogarai/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','YumJournal.pythonanywhere.com']
 
 
 # Application definition
